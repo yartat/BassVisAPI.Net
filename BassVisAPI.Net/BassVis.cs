6a1ee7b378acc8277ea21f7be37e2b6fd2a41e65
Yaroslav V Tatarenko <yaroslavtatarenko@gmail.com>
2017-06-18 13:55:21 +0300

    Fix build platform

--- BassVisAPI.Net/BassVis.cs
+++ BassVisAPI.Net/BassVis.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.InteropServices;
 
 using JetBrains.Annotations;
@@ -9,18 +10,30 @@ namespace BassVisAPI
     [PublicAPI]
     public sealed class BassVis
     {
+        private const string BassVisFileName = "bass_vis.dll";
+
+        static BassVis()
+        {
+            if (!File.Exists(BassVisFileName))
+            {
+                NativeMethods.LoadLibraryEx(
+                    Path.Combine(Environment.Is64BitProcess ? @".\x64" : @".\x86", BassVisFileName),
+                    IntPtr.Zero,
+                    NativeMethods.LoadLibraryFlags.DEFAULT);
+            }
+        }
+
         public delegate void BassVisState(BASSVIS_PLAYSTATE state);
 
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
-
         public static extern bool BASSVIS_AIMP2VIS_RenderDeviceToDC(
             BASSVISKind kind, 
             IntPtr handle, 
             int channel, 
             IntPtr hDc);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool BASSVIS_AIMP2VIS_RenderStreamToDC(
             BASSVISKind kind, 
@@ -28,78 +41,78 @@ namespace BassVisAPI
             int channel, 
             IntPtr hDc);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool BASSVIS_Clicked(BASSVIS_PARAM param, int x, int y);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool BASSVIS_Config(BASSVIS_PARAM param, int module);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         public static extern void BASSVIS_ExecutePlugin(BASSVIS_EXEC exec, BASSVIS_PARAM param);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         private static extern IntPtr BASSVIS_FindPlugins(
             BASSVISKind kind, 
             [In, MarshalAs(UnmanagedType.LPStr)] string pluginPath, 
             BASSVISGetPlugin findType, 
             byte param);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern void BASSVIS_Free(BASSVIS_PARAM param);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         public static extern int BASSVIS_GetModuleHandle(
             BASSVISKind kind, 
             [In, MarshalAs(UnmanagedType.LPStr)] string pluginPath);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         public static extern int BASSVIS_GetModulePresetCount(
             BASSVIS_PARAM param, 
             [In, MarshalAs(UnmanagedType.LPStr)] string pluginPath);
 
-        [DllImport("bass_vis.dll", EntryPoint="BASSVIS_GetModulePresetNameList", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, EntryPoint="BASSVIS_GetModulePresetNameList", CharSet=CharSet.Auto)]
         private static extern IntPtr BASSVIS_GetModulePresetNameListPtr(
             BASSVIS_PARAM param, 
             [In, MarshalAs(UnmanagedType.LPStr)] string pluginPath);
 
-        [DllImport("bass_vis.dll", EntryPoint="BASSVIS_GetModulePresetName", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, EntryPoint="BASSVIS_GetModulePresetName", CharSet=CharSet.Auto)]
         private static extern IntPtr BASSVIS_GetModulePresetNamePtr(
             BASSVIS_PARAM param, 
             int module, 
             [In, MarshalAs(UnmanagedType.LPStr)] string pluginPath);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         public static extern int BASSVIS_GetOption(BASSVIS_PARAM param, BASSVIS_CONFIGFLAGS option);
 
-        [DllImport("bass_vis.dll", EntryPoint="BASSVIS_GetPluginName", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, EntryPoint="BASSVIS_GetPluginName", CharSet=CharSet.Auto)]
         private static extern IntPtr BASSVIS_GetPluginNamePtr(BASSVIS_PARAM param);
 
-        [DllImport("bass_vis.dll", EntryPoint="BASSVIS_GetVersion", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, EntryPoint="BASSVIS_GetVersion", CharSet=CharSet.Auto)]
         private static extern IntPtr BASSVIS_GetVersionPtr();
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool BASSVIS_Init(BASSVISKind kind, IntPtr handle);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool BASSVIS_IsFree(BASSVIS_PARAM param);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern void BASSVIS_Quit(BASSVIS_PARAM param);
 
         [return: MarshalAs(UnmanagedType.Bool)]
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         public static extern bool BASSVIS_RenderChannel(BASSVIS_PARAM param, int channel, bool usewasapi);
         [return: MarshalAs(UnmanagedType.Bool)]
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         public static extern bool BASSVIS_Resize(BASSVIS_PARAM param, int left, int top, int width, int height);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool BASSVIS_SetFullscreen(
             BASSVIS_PARAM param, 
@@ -113,26 +126,26 @@ namespace BassVisAPI
             int screenWidth, 
             int screenHeight);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool BASSVIS_SetInfo(BASSVIS_PARAM param, BASSVIS_INFO info);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool BASSVIS_SetModulePreset(BASSVIS_PARAM param, int index);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool BASSVIS_SetOption(BASSVIS_PARAM param, BASSVIS_CONFIGFLAGS option, int value);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         public static extern int BASSVIS_SetPlayState(
             BASSVIS_PARAM param, 
             BASSVIS_PLAYSTATE state, 
             int value, 
             [MarshalAs(UnmanagedType.LPStr)] string title);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         public static extern void BASSVIS_SetVisPort(
             BASSVIS_PARAM param, 
             IntPtr windowHandle, 
@@ -142,17 +155,17 @@ namespace BassVisAPI
             int width, 
             int height);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool BASSVIS_StartRecord(BASSVIS_PARAM param, int samplerate, int channels);
 
-        [DllImport("bass_vis.dll", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, CharSet=CharSet.Auto)]
         public static extern void BASSVIS_WINAMP_VisButtonClicked(BASSVIS_PARAM param, BASSVIS_BUTTONFLAGS button);
 
-        [DllImport("bass_vis.dll", EntryPoint="BASSVIS_WINAMP_RemoveCallback", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, EntryPoint="BASSVIS_WINAMP_RemoveCallback", CharSet=CharSet.Auto)]
         public static extern void BASSVIS_WINAMPRemoveCallback();
 
-        [DllImport("bass_vis.dll", EntryPoint="BASSVIS_WINAMP_SetStateCallback", CharSet=CharSet.Auto)]
+        [DllImport(BassVisFileName, EntryPoint="BASSVIS_WINAMP_SetStateCallback", CharSet=CharSet.Auto)]
         public static extern void BASSVIS_WINAMPSetStateCallback(BassVisState proc);
 
         public static bool StartRecord(BASSVIS_PARAM param, int samplerate = 44100, int channels = 2) =>
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![NuGet status](https://img.shields.io/nuget/v/Bass_Vis.NET.svg)](https://www.nuget.org/packages/Bass_Vis.NET)
+[![Build status](https://ci.appveyor.com/api/projects/status/67ubhtmijuhyhq6q?svg=true)](https://ci.appveyor.com/project/yartat/bassvisapi-net)
+--
 # BassVisAPI.Net
 .NET wrapper for BASS_VIS addon
